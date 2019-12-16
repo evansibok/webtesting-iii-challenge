b09c42806c72429b6fea198bebe804234865a2a4
Evans Ibok <evansibok@gmail.com>
2019-12-16 22:55:11 +0100

    test done for dashboard component

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -11,6 +11,37 @@ beforeEach(() => {
   wrapper = rtl.render(<Dashboard />)
 });
 
-it('renders without crashing', () => {
-  expect(wrapper.container).toMatchSnapshot();
+const unlocked = () => wrapper.queryByText(/Unlocked/i);
+const open = () => wrapper.queryByText(/Open/i);
+const lockGate = () => wrapper.queryByText(/Lock Gate/i);
+const closeGate = () => wrapper.queryByText(/Close Gate/i);
+const closed = () => wrapper.queryByText(/Closed/i);
+const openGate = () => wrapper.queryByText(/Open Gate/i);
+const locked = () => wrapper.queryByText(/Locked/i);
+const unlockGate = () => wrapper.queryByText(/Unlock Gate/i);
+
+
+describe('Dashboard Component renders without crashing', () => {
+  it('matches previous snapshot', () => {
+    expect(wrapper.container).toMatchSnapshot();
+  })
+
+  it('displays initial components correctly', () => {
+    expect(unlocked()).toBeInTheDocument();
+    expect(open()).toBeInTheDocument();
+    expect(lockGate()).toBeInTheDocument();
+    expect(closeGate()).toBeInTheDocument();
+    expect(lockGate()).toBeDisabled();
+  })
+})
+
+describe('Close Gate works properly', () => {
+
+  it('on "Close Gate" click "Open" changes to "Closed"', () => {
+    rtl.fireEvent.click(closeGate());
+    expect(closed()).toBeInTheDocument();
+    expect(openGate()).toBeInTheDocument();
+    expect(lockGate()).not.toBeDisabled();
+    expect(wrapper.container).toMatchSnapshot();
+  })
 })
