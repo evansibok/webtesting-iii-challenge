9582c01dc8ff6f701148eb140631d2adaec5dfd5
Evans Ibok <evansibok@gmail.com>
2019-12-16 16:09:39 +0100

    started project

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,16 @@
 // Test away
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'
+import Dashboard from './Dashboard'
+
+afterEach(rtl.cleanup);
+
+let wrapper;
+beforeEach(() => {
+  wrapper = rtl.render(<Dashboard />)
+});
+
+it('renders without crashing', () => {
+  expect(wrapper.container).toMatchSnapshot();
+})
